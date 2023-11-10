6c865a99a1772d735b960b260307b584c329bea9
Daniel Nogueira <148869640+DanielNogueira8859@users.noreply.github.com>
2023-11-10 17:00:24 -0300

    Add files via upload

--- aula5.py
+++ aula5.py
@@ -8,25 +8,54 @@
 # for key, valor in menino.items():
 #     print(key,valor)
 #---------------------------------------------------------
-for numero in range(2, 21, 2):
-    print(numero)
+# for numero in range(2, 21, 2):
+#     print(numero)
 
-for multiplos in range(5,55,5):
-    print(multiplos)
-
-
-variavel = str("meu Pil")
-tipo = type(variavel)
-print(tipo)
+# for multiplos in range(5,55,5):
+#     print(multiplos)
 
 
-nome = input("escreva o nome")
-print(f"bem vindo {nome}")
+# variavel = str("meu Pil")
+# tipo = type(variavel)
+# print(tipo)
+
+
+# nome = input("escreva o nome")
+# print(f"bem vindo {nome}")
 
 
 
 
-for i in range(1,11,2):
-    print(i)
+# for i in range(1,11,2):
+#     print(i)
+
+# n = [range(1,50),1]
+# add = n.append(10)
+# add = n.pop()
+# print(n)
+
+#--------------------------------------------------------------------Exercicios 1 até 4
+
+# numeros = [1,2,3,4,5]
+# print(numeros)
 
 
+
+# numeros = [1,2,3,4,5]
+# for i in numeros:
+#     print(i)
+#     numeros.append(6)
+#     print(numeros)
+    
+
+#     numeros.pop(2)
+#     print(numeros)
+#     break
+#===================================================================
+
+frutas = ["maça", "pera","goiaba"]
+numeros = [1,2,3]
+comprimento = len(numeros)
+for i in range(comprimento):
+    print(f"o numero da sua fruta é {numeros[i]} e a sua fruta é {frutas[i]}")
+    
