e5e67aeeccb211dc3e1b2ad7b08605edbfbfd303
Daniel Nogueira <148869640+DanielNogueira8859@users.noreply.github.com>
2023-11-10 15:11:48 -0300

    Update aula4.py

--- aula4.py
+++ aula4.py
@@ -1,124 +1,126 @@
-# #----------------------------------------------------------------------------------
-# print('digite um numero')
-# idade1 = 45
-# print('digite um numero')
-# idade2 = 18
+#----------------------------------------------------------------------------------
+print('digite um numero')
+idade1 = 45
+print('digite um numero')
+idade2 = 18
 
-# if idade1 >= idade2:
-#     print("A primeira idade é maior.")
-# elif idade1 < idade2:
-#     print("A segunda idade é maior.")
-# else:
-#     print("As idades são iguais.")
-# #----------------------------------------------------------------------------------
-# idade_cliente = int(input("Digite a idade do cliente: "))
+if idade1 >= idade2:
+    print("A primeira idade é maior.")
+elif idade1 < idade2:
+    print("A segunda idade é maior.")
+else:
+    print("As idades são iguais.")
+#----------------------------------------------------------------------------------
+idade_cliente = int(input("Digite a idade do cliente: "))
 
-# if idade_cliente < 18:
-#     print("Desculpe, entrada permitida apenas para maiores de 18 anos.")
-# else:
-#     print("Bem-vindo ao show!")
-# #----------------------------------------------------------------------------------
+if idade_cliente < 18:
+    print("Desculpe, entrada permitida apenas para maiores de 18 anos.")
+else:
+    print("Bem-vindo ao show!")
+#----------------------------------------------------------------------------------
 
-# nota1 = float(input('Digite a primeira nota: '))
-# nota2 = float(input('Digite a segunda nota: '))
-# nota3 = float(input('Digite a terceira nota: '))
+nota1 = float(input('Digite a primeira nota: '))
+nota2 = float(input('Digite a segunda nota: '))
+nota3 = float(input('Digite a terceira nota: '))
 
-# media  = nota1 + nota2 + nota3
-# media = media/3
+media  = nota1 + nota2 + nota3
+media = media/3
 
-# if media > 6.0:
-#     print('vc passou',media)
+if media > 6.0:
+    print('vc passou',media)
 
-# else:
-#     print('vc nao passou, pq sua média foi',media)
-#-----------------------------------------------------------------------------------------
-# var1=  123
-# var2 ='World'
-# print("Hello to the", var2, var1)
+else:
+    print('vc nao passou, pq sua média foi',media)
 
-# var1 =  123
-# var2 = 'World'
-# print("Hello to the %s %d "%(var2, var1))
+var1=  123
+var2 ='World'
+print("Hello to the", var2, var1)
 
-# var1 = 123
-# var2 = 'World'
-# print("Hello to the {}".format(var2, var1))
+var1 =  123
+var2 = 'World'
+print("Hello to the %s %d "%(var2, var1))
 
-# var1= 123
-# var2 = 'World'
-# print("Hello to the " + var2+ str(var1))
+var1 = 123
+var2 = 'World'
+print("Hello to the {}".format(var2, var1))
 
-# var1 =123
-# var2 ='World'
-# print (f"Hello to the {var2} {var1} ")
+var1= 123
+var2 = 'World'
+print("Hello to the " + var2+ str(var1))
 
-#------------------------Exercício 1: Verificação de Número Positivo---------------------
-# print("digite um numero")
-# numero = int(input('digite o seu numero'))
+var1 =123
+var2 ='World'
+print (f"Hello to the {var2} {var1} ")
 
-# if numero > 0:
-#     print("numero maior que zero")
-# elif numero <= 0:
-#     print("numero menor que zero ou igual a zero")
-
-# else:
-#     print("numero invalido")
+print("------------------------Exercício 1: Verificação de Número Positivo---------------------")
 
 
-#---------------------Exercício 2: Classificação de Triângulos------------------------------
+print("digite um numero")
+numero = int(input('digite o seu numero'))
 
-# medida1= int(input("digite a medida"))
-# medida2= int(input("digite a medida"))
-# medida3= int(input("digite a medida"))
+if numero > 0:
+    print("numero maior que zero")
+elif numero <= 0:
+    print("numero menor que zero ou igual a zero")
 
-# equilatero = medida1 or medida2 or medida3
-
-
-
-# if medida1 == medida2 and medida1 == medida3 and medida2 == medida1 and medida2== medida3 and medida3 == medida1 and medida3 == medida2 :
-#     print("seu triangulo é equilátero")
+else:
+    print("numero invalido")
 
 
-# elif medida1 != medida2 and medida1 !=medida3 and medida2 != medida1 and medida2 != medida3 and medida3 != medida1 and medida3 != medida2 :
-#     print("seu triangulo é escaleno")
+print("---------------------Exercício 2: Classificação de Triângulos------------------------------")
 
-# elif medida1 or medida2 or medida3 != medida1 or medida1 or medida2 or medida3 != medida2 or medida1 or medida2 or medida3 != medida3:
-#     print("seu triangulo é isósceles")
+medida1= int(input("digite a medida"))
+medida2= int(input("digite a medida"))
+medida3= int(input("digite a medida"))
 
-# else :
-#     print("Seu triangulo nao sei qual é ")
+equilatero = medida1 or medida2 or medida3
+
+
+
+if medida1 == medida2 and medida1 == medida3 and medida2 == medida1 and medida2== medida3 and medida3 == medida1 and medida3 == medida2 :
+    print("seu triangulo é equilátero")
+
+
+elif medida1 != medida2 and medida1 !=medida3 and medida2 != medida1 and medida2 != medida3 and medida3 != medida1 and medida3 != medida2 :
+    print("seu triangulo é escaleno")
+
+elif medida1 or medida2 or medida3 != medida1 or medida1 or medida2 or medida3 != medida2 or medida1 or medida2 or medida3 != medida3:
+    print("seu triangulo é isósceles")
+
+else :
+    print("Seu triangulo nao sei qual é ")
     
-#--------------------------------------Metodos----------------------------------------------
-#len = comprimento
-# text = "hello word"
 
-# comprimento = len(text)
-# print(comprimento)
+len = comprimento
+text = "hello word"
 
-# #type
+comprimento = len(text)
+print(comprimento)
 
-# nuber = 100
-# tipo = type(nuber)
-# print(tipo)
+#type
+
+nuber = 100
+tipo = type(nuber)
+print(tipo)
 
 
-# #conversao
+#conversao
 
-# n1 = '10'
-# n2 =15.0
-# n3 = 25
+n1 = '10'
+n2 =15.0
+n3 = 25
 
-# print("------------------------------------------------------")
+print("------------------------------------------------------")
 
-# v1 = int(n1)
-# print(type(v1))
+v1 = int(n1)
+print(type(v1))
 
-# v2=  str(n2)
-# print(type(v2))
+v2=  str(n2)
+print(type(v2))
 
-# v3 = float(n3)
-# print(type(v3))
-#----------------------------------------------------------------------------------------------
+v3 = float(n3)
+print(type(v3))
+
 
 
 text = "ola mundo"
